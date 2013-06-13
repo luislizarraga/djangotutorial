c726a625ddae1ccf26c029a10ac17157c1268986
Luis Lizarraga <siul.al4@gmail.com>
2013-06-13 11:50:21 -0500

    modified admin.py added class PollAdmin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from polls.models import Poll
 
-admin.site.register(Poll)
+
+
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+admin.site.register(Poll, PollAdmin)
+
+
+# admin.site.register(Poll) lo primero que tenia
