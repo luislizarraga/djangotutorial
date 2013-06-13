aefcbc19692e96b86bfdc887f07a9d66dd1bf23d
Luis Lizarraga <siul.al4@gmail.com>
2013-06-13 13:04:56 -0500

    fourth modification to admin.py, added class ChoiceInLine, working.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,36 +1,15 @@
 from django.contrib import admin
 from polls.models import Choice, Poll
 
-
-#class ChoiceInline(admin.StackedInline):
-#    model = Choice
-#    extra = 3
-
-
-
-
-
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,              {'fields': ['question']}),
-        ('Date Information',{'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-#    inlines = [ChoiceInline]
-
-
-#fieldsets = [
-#        (None,              {'fields': ['question']}),     lo tercero que tenia
-#        ('Date Information',{'fields': ['pub_date']}),
-#    ]
-
-
-
-#    fields = ['pub_date', 'question'] lo segundo que tenia dentro de la clase
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
-
-
-
-# admin.site.register(Poll) lo primero que tenia
