7f7c5687e383e5dcade184143b9f6a89648b1673
Luis Lizarraga <siul.al4@gmail.com>
2013-06-13 12:08:51 -0500

    second modification to admin.py

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,9 +4,15 @@ from polls.models import Poll
 
 
 class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
+    fieldsets = [
+        (None,              {'fields': ['question']}),
+        ('Date Information',{'fields': ['pub_date']}),
+    ]
+#    fields = ['pub_date', 'question'] lo segundo que tenia dentro de la clase
 
 admin.site.register(Poll, PollAdmin)
+
+
 
 
 # admin.site.register(Poll) lo primero que tenia
