0c27d68a42d79ac1e63964ffd92fc37e2de3ab91
Luis Lizarraga <siul.al4@gmail.com>
2013-06-13 17:02:45 -0500

    Starting with views.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@
 
 from django.http import HttpResponse
 #from django.template import Context, loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from polls.models import Poll
 
 #def index(request):
@@ -27,7 +27,8 @@ def index(request):
 
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
